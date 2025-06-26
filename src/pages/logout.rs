f40ef16052b4ff03a4e0d7bb3ebd3909246bef10
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2025-06-26 20:21:34 +0200

    🐛 Delete session on logout

--- src/pages/logout.rs
+++ src/pages/logout.rs
@@ -1,14 +1,30 @@
 use poem::{
     handler,
-    web::{cookie::CookieJar, Redirect},
+    web::{cookie::CookieJar, Data, Redirect},
     IntoResponse,
 };
+use sea_orm::{DatabaseConnection, EntityTrait};
 
-use crate::{session, BASE_PATH};
+use crate::{
+    internal_server_error,
+    models::Sessions,
+    session::{self},
+    BASE_PATH,
+};
 
 #[handler]
-pub async fn get(cookie_jar: &CookieJar) -> anyhow::Result<poem::Response> {
-    cookie_jar.remove(session::PATROL_COOKIE);
+pub async fn get(
+    Data(db): Data<&DatabaseConnection>,
+    cookie_jar: &CookieJar,
+) -> poem::Result<poem::Response> {
+    if let Some(cookie) = cookie_jar.get(session::PATROL_COOKIE) {
+        Sessions::delete_by_id(cookie.value_str())
+            .exec(db)
+            .await
+            .map_err(internal_server_error)?;
+
+        cookie_jar.remove(session::PATROL_COOKIE);
+    }
 
     Ok(Redirect::see_other(BASE_PATH.to_string() + "/login").into_response())
 }
